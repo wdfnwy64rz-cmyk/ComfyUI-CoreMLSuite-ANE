e4926716da6e3135fb67f0a2287b7e31d89943cf
wdfnwy64rz-cmyk <wdfnwy64rz@privaterelay.appleid.com>
2025-11-23 14:22:41 +0700

    Add ANE-optimized SDXL UNet wrapper

--- coreml_suite/converter.py
+++ coreml_suite/converter.py
@@ -15,9 +15,10 @@ from diffusers import (
 )
 from python_coreml_stable_diffusion.unet import (
     UNet2DConditionModel,
-    UNet2DConditionModelXL,
     AttentionImplementations,
 )
+
+from coreml_suite.unet_sdxl_ane import build_sdxl_unet_for_ane
 
 from coreml_suite.config import ModelVersion
 from coreml_suite.lcm.unet import UNet2DConditionModelLCM
@@ -31,7 +32,7 @@ class StableDiffusionLCMPipeline(LatentConsistencyModelPipeline):
 
 MODEL_TYPE_TO_UNET_CLS = {
     ModelVersion.SD15: UNet2DConditionModel,
-    ModelVersion.SDXL: UNet2DConditionModelXL,
+    ModelVersion.SDXL: build_sdxl_unet_for_ane,
     ModelVersion.LCM: UNet2DConditionModelLCM,
 }
 
@@ -45,8 +46,18 @@ MODEL_TYPE_TO_PIPE_CLS = {
 def get_unet(model_type: ModelVersion, ref_pipe):
     ref_unet = ref_pipe.unet
 
-    unet_cls = MODEL_TYPE_TO_UNET_CLS[model_type]
-    cml_unet = unet_cls.from_config(ref_unet.config).eval()
+    unet_factory = MODEL_TYPE_TO_UNET_CLS[model_type]
+
+    if model_type is ModelVersion.SDXL:
+        cml_unet = unet_factory(
+            ref_unet.config,
+            attention_impl=AttentionImplementations(
+                python_coreml_stable_diffusion.unet.ATTENTION_IMPLEMENTATION_IN_EFFECT
+            ),
+        )
+    else:
+        cml_unet = unet_factory.from_config(ref_unet.config).eval()
+
     cml_unet.load_state_dict(ref_unet.state_dict(), strict=False)
 
     return cml_unet
